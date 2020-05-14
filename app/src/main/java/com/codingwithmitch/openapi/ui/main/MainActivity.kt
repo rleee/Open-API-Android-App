bc12a64eb803e2d5a2a5712ec033b7aaef3036a8
rleee <danlee0150@gmail.com>
2020-05-14 17:04:14 +0800

    Custom Bottom Nav Controller

--- app/src/main/java/com/codingwithmitch/openapi/ui/main/MainActivity.kt
+++ app/src/main/java/com/codingwithmitch/openapi/ui/main/MainActivity.kt
@@ -3,25 +3,51 @@ package com.codingwithmitch.openapi.ui.main
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.MenuItem
 import android.view.View
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import androidx.navigation.NavController
+import androidx.navigation.ui.setupWithNavController
 import com.codingwithmitch.openapi.R
 import com.codingwithmitch.openapi.ui.BaseActivity
 import com.codingwithmitch.openapi.ui.auth.AuthActivity
+import com.codingwithmitch.openapi.ui.main.account.ChangePasswordFragment
+import com.codingwithmitch.openapi.ui.main.account.UpdateAccountFragment
+import com.codingwithmitch.openapi.ui.main.blog.UpdateBlogFragment
+import com.codingwithmitch.openapi.ui.main.blog.ViewBlogFragment
+import com.codingwithmitch.openapi.util.BottomNavController
+import com.codingwithmitch.openapi.util.setUpNavigation
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : BaseActivity() {
+class MainActivity : BaseActivity(),
+    BottomNavController.NavGraphProvider,
+    BottomNavController.OnNavigationGraphChanged,
+    BottomNavController.OnNavigationReselectedListener {
+
+    private lateinit var bottomNavigationView: BottomNavigationView
+    private val bottomNavController by lazy(LazyThreadSafetyMode.NONE) {
+        BottomNavController(
+            this,
+            R.id.main_nav_host_fragment,
+            R.id.nav_blog,
+            this, this
+        )
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        subscribeObserver()
-
-        // will remove later, only for testing to logout
-        tool_bar.setOnClickListener {
-            sessionManager.logout()
+        setupActionBar()
+        bottomNavigationView = findViewById(R.id.bottom_navigation_view)
+        bottomNavigationView.setUpNavigation(bottomNavController, this)
+        if (savedInstanceState == null) {
+            bottomNavController.onNavigationItemSelected()
         }
+
+        subscribeObserver()
     }
 
     override fun displayProgressBar(boolean: Boolean) {
@@ -45,5 +71,42 @@ class MainActivity : BaseActivity() {
         val intent = Intent(this, AuthActivity::class.java)
         startActivity(intent)
         finish()
+    }
+
+    private fun setupActionBar() {
+        setSupportActionBar(tool_bar)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            android.R.id.home -> onBackPressed() // android.R.id.home is The Up Button
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onBackPressed() {
+        bottomNavController.onBackPressed()
+    }
+
+    override fun getNavGraphId(itemId: Int): Int {
+        return when (itemId) {
+            R.id.nav_blog -> R.navigation.nav_blog
+            R.id.nav_account -> R.navigation.nav_account
+            R.id.nav_create_blog -> R.navigation.nav_create_blog
+            else -> R.navigation.nav_blog
+        }
+    }
+
+    override fun onGraphChange() {
+        // later
+    }
+
+    override fun onReselectNavItem(navController: NavController, fragment: Fragment) {
+        when (fragment) {
+            is ViewBlogFragment -> navController.navigate(R.id.action_viewBlogFragment_to_blogFragment)
+            is UpdateBlogFragment -> navController.navigate(R.id.action_updateBlogFragment_to_blogFragment)
+            is UpdateAccountFragment -> navController.navigate(R.id.action_updateAccountFragment_to_accountFragment)
+            is ChangePasswordFragment -> navController.navigate(R.id.action_changePasswordFragment_to_accountFragment)
+        }
     }
 }
